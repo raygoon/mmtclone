c40fa01a62983870f92241e0d426f5239a74c7b4
raygoon <jasonraycat@gmail.com>
2021-09-05 23:17:18 +0900

    update

--- js/background.js
+++ js/background.js
@@ -1,6 +1,16 @@
 const images = ["a.jpg","b.jpg","c.jpg","d.jpg","e.jpg","f.jpg","g.jpg"];
 const randImg =images[Math.floor(Math.random() * images.length)];
 const viewBG = document.createElement("img");
-viewBG.src = `img/${randImg}`;
 
-document.body.appendChild(viewBG);
+viewBG.src = `img/${randImg}`;
+viewBG.className = "bgimg";
+
+window.onload = function(){
+    const backImg = document.getElementById("body").style;
+    backImg.backgroundRepeat = "repeat";
+    backImg.backgroundSize = "cover";
+    backImg.backgroundImage =`url(${viewBG.src})`;
+}
+
+
+// document.body.appendChild(viewBG);
