9a573bc5ea5fa8df0197f3816b96079922452b51
raygoon <jasonraycat@gmail.com>
2021-10-02 00:54:13 +0900

    c

--- js/background.js
+++ js/background.js
@@ -1,18 +1,21 @@
 const images = ["a.jpg","b.jpg","c.jpg","d.jpg","e.jpg","f.jpg","g.jpg","h.jpg","i.jpg","m.jpg"];
-const randImg =images[Math.floor(Math.random() * images.length)];
+const randImg = images[Math.floor(Math.random() * images.length)];
 const viewLowBG = document.createElement("img");
 const viewBG = document.createElement("img");
+let backImg = document.getElementById("body").style;
 
 viewLowBG.src = `img/low_img/${randImg}`;
-console.log(viewLowBG.src);
 viewBG.src = `img/${randImg}`;
 viewBG.className = "bgimg";
 
 window.onload = function(){
-    const backImg = document.getElementById("body").style;
     backImg.backgroundRepeat = "repeat";
     backImg.backgroundSize = "cover";
-    backImg.backgroundImage =`url(${viewLowBG.src}), url(${viewBG.src})`;
+    backImg.backgroundImage =`url(${viewLowBG.src})`;
+    changeHighRes();
+    // setTimeout(changeHighRes, 1000);
 }
 
-// document.body.appendChild(viewBG);
+function changeHighRes() {
+    backImg.backgroundImage =`url(${viewBG.src})`;
+}
