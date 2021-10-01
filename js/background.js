70fb76227ffa8b941eb3ad8b621c4646eb8ec6f5
raygoon <jasonraycat@gmail.com>
2021-10-02 02:38:30 +0900

    rr

--- js/background.js
+++ js/background.js
@@ -1,26 +1,26 @@
 const images = ["a.jpg","b.jpg","c.jpg","d.jpg","e.jpg","f.jpg","g.jpg","h.jpg","i.jpg","m.jpg"];
 const randImg = images[Math.floor(Math.random() * images.length)];
-const viewLowBG = document.createElement("img");
 const viewBG = document.createElement("img");
 let backImg = document.getElementById("body");
-
-viewLowBG.src = `img/low_img/${randImg}`;
-viewBG.src = `img/${randImg}`;
 viewBG.className = "bgimg";
 
-window.onload = function(){
+function backImgLoad(){
+    viewBG.src = `img/low_img/${randImg}`;
     backImg.style.backgroundRepeat = "repeat";
     backImg.style.backgroundSize = "cover";
-    backImg.style.backgroundImage =`url(${viewLowBG.src})`;
-    console.log(backImg.style.backgroundImage);
+    backImg.style.backgroundImage =`url(${viewBG.src})`;
 }
 
-function waitForImageToLoad(imageElement){
-    return new Promise(resolve=>{imageElement.onload = resolve})
+document.addEventListener("DOMContentLoaded",backImgLoad);
+
+window.onload = function(){
+    viewBG.src = `img/${randImg}`;
+    console.log("윈도우로드");
+    backImg.style.backgroundRepeat = "repeat";
+    backImg.style.backgroundSize = "cover";
+    backImg.style.backgroundImage =`url(${viewBG.src})`;
 }
 
-waitForImageToLoad(viewLowBG).then(()=>{
-    viewLowBG.src = `img/${randImg}`;
-    return new Promise(resolve=>{viewLowBG.onload = resolve});
-});
+
+
 
